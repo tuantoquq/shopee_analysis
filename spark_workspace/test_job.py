5b214eee7ca486e1c49cc90dc81f753beaf4c03a
tuannha <tuantoquq@gmail.com>
2022-01-23 01:32:18 +0700

    last commit

--- spark_workspace/test_job.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.appName('Job Test').getOrCreate()
-df = spark.read.parquet('hdfs://namenode:9000/user/tuannha/data/test.parquet')
-df.printSchema()
-print(df.count())
